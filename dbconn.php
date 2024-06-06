fa67cea526feb5bf9b954cdb3a8a3a257884938f
amirahim17 <amirahimm17@gmail.com>
2024-06-07 03:12:38 +0800

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -3,7 +3,7 @@
 	$user = "root"; 			//mysqlusername
 	$pass = ""; 				//mysqlpassword
 	$host = "localhost"; 		//server name or ipaddress //127.0.0.1
-	$dbname = "medichubdb"; 	//your db name
+	$dbname = "newmedichubdb"; 	//your db name
 
 	try {
     	$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
